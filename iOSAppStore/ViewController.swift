c72f48cab78f574af2e0b583f50676135185a340
Ky Nguyen <kynguyen@linecorp.com>
2019-03-20 11:11:56 +0700

    Initial Commit

---
+++ iOSAppStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSAppStore
+//
+//  Created by Ky Nguyen on 3/20/19.
+//  Copyright © 2019 Ky Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
